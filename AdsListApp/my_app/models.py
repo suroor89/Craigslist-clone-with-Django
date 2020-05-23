9df64d25eec14497919e3e6bc715a31ac2af3f62
suroor89 <bravalji@gmail.com>
2020-05-23 16:38:31 -0400

    added Procfile file heroku deployment and migrate database

--- AdsListApp/my_app/models.py
+++ AdsListApp/my_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Search(models.Model):
-    search = models.CharField(max_length=500)
+    search = models.CharField(max_length=500, null=True)
     created = models.DateTimeField(auto_now=True)
 
     def __str__(self):
